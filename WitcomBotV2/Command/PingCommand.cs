a11dd3e19215655dce130cdc455a49fbbfdf5e2e
User_NotExist <49120419+UserNonExist@users.noreply.github.com>
2023-05-07 23:37:11 +0700

    PingCommand Addition

---
+++ WitcomBotV2/Command/PingCommand.cs
@@ -0,0 +1,15 @@
+﻿using Discord;
+using Discord.Interactions;
+using WitcomBotV2.Service;
+
+namespace WitcomBotV2.Command;
+
+public class PingCommand : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("ping", "ดู latency ของบอท")]
+    public async Task Ping()
+    {
+        await RespondAsync(embed: await EmbedBuilderService.CreateBasicEmbed("Ping", $"latency อยู่ที่ {Context.Client.Latency}ms" , Color.Gold),
+            ephemeral: false);
+    }
+}
