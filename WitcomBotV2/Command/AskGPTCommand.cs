7b8c48fd062e4706c071ac11cd630b6dcb088696
User_NotExist <49120419+UserNonExist@users.noreply.github.com>
2023-06-10 20:15:34 +0700

    askGPT redundant
    I do not have the damn money to pay to you openAI

--- WitcomBotV2/Command/AskGPTCommand.cs
+++ WitcomBotV2/Command/AskGPTCommand.cs
@@ -9,17 +9,22 @@ namespace WitcomBotV2.Command;
 public class AskGPTCommand : InteractionModuleBase<ShardedInteractionContext>
 {
     [DefaultMemberPermissions(GuildPermission.SendMessages)]
-    [SlashCommand("askgpt", "ถาม GPT-3")]
+    [SlashCommand("askgpt", "DNI ถาม GPT-3")]
     public async Task AskGPT([Discord.Interactions.Summary("Question", "คำถาม")] [Remainder] string question)
     {
-        await DeferAsync();
-        try
-        {
-            await FollowupAsync(embed: await EmbedBuilderService.CreateBasicEmbed("GPT-3", GPTModule.AskGPT3(question).Result, Color.Blue));
-        }
-        catch (Exception e)
-        {
-            await FollowupAsync(embed: await ErrorHandlingService.GetErrorEmbed(ErrorCodes.Unspecified, e.Message));
-        }
+        // await DeferAsync();
+        // try
+        // {
+        //     await FollowupAsync(embed: await EmbedBuilderService.CreateBasicEmbed("GPT-3", GPTModule.AskGPT3(question).Result, Color.Blue));
+        // }
+        // catch (Exception e)
+        // {
+        //     await FollowupAsync(embed: await ErrorHandlingService.GetErrorEmbed(ErrorCodes.Unspecified, e.Message));
+        // }
+
+        await RespondAsync(
+            embed: await EmbedBuilderService.CreateBasicEmbed("GPT-3",
+                "ตอนนี้ระบบ AskGPT ถูกปิดใช้งานเนื่องจากว่าเจ้าของบอทไม่มีเงินจ่ายค่า API\n\nขออภัยในความไม่สะดวก",
+                Color.Purple), ephemeral: true);
     }
 }
